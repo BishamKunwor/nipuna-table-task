224fc7bdc99041fe40abf8abd3074d4091c32c91
Bisham Kunwor <novelian.nova@gmail.com>
2023-09-21 05:31:07 +0545

    Added Hover Effect In NavBar

--- src/components/NavBar/DashboardConfig.tsx
+++ src/components/NavBar/DashboardConfig.tsx
@@ -1,38 +1,48 @@
+const navBarMenu = [
+  {
+    src: '/icons/plus.svg',
+    title: 'Add Task',
+    className: ''
+  },
+  {
+    src: '/icons/bell-02.svg',
+    title: 'Notifications',
+    className: ''
+  },
+  {
+    src: '/icons/mail-01.svg',
+    title: 'Mails',
+    className: ''
+  },
+  {
+    src: '/icons/settings-02.svg',
+    title: 'Settings',
+    className: ''
+  },
+  {
+    src: '/icons/line.svg',
+    title: 'Line',
+    className: 'hover:bg-transparent cursor-default'
+  },
+  {
+    src: '/icons/moon-01.svg',
+    title: 'Toggle Theme',
+    className: ''
+  }
+]
 export default function DashboardConfig () {
   return (
     <main className='flex gap-6 max-h-10'>
-      <div className='flex gap-6'>
-        <img
-          src='/icons/plus.svg'
-          className='md:cursor-pointer'
-          title='Add Task'
-        />
-        <img
-          src='/icons/bell-02.svg'
-          className='md:cursor-pointer'
-          title='Notifications'
-        />
-        <img
-          src='/icons/mail-01.svg'
-          className='md:cursor-pointer'
-          title='Mails'
-        />
-        <img
-          src='/icons/settings-02.svg'
-          className='md:cursor-pointer'
-          title='Settings'
-        />
-        <img
-          src='/icons/line.svg'
-          className='md:cursor-pointer'
-          title='Settings'
-        />
-        <img
-          src='/icons/moon-01.svg'
-          className='md:cursor-pointer'
-          title='Settings'
-        />
-        <div className='flex gap-[10px]'>
+      <div className='flex gap-4 items-center'>
+        {navBarMenu.map((data, index) => (
+          <img
+            key={index}
+            src={data.src}
+            className={`md:cursor-pointer p-1 hover:bg-[var(--hover)] transition-all ${data.className}`}
+            title={data.title}
+          />
+        ))}
+        <div className='flex gap-[10px] items-center'>
           <img
             src='/user-images/nav-bar-user.png'
             className='md:cursor-pointer h-10 w-10 rounded-full object-cover'
@@ -40,7 +50,7 @@ export default function DashboardConfig () {
           />
           <img
             src='/icons/elements.svg'
-            className='md:cursor-pointer'
+            className='md:cursor-pointer p-1 py-2 hover:bg-[var(--hover)] transition-all'
             title='Settings'
           />
         </div>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,7 +4,7 @@ export default function NavBar () {
   return (
     <nav className='px-8 max-h-20 min-h-[72px] flex items-center bg-white justify-between'>
       <aside className='flex gap-4'>
-        <button>
+        <button className='hover:bg-[var(--hover)] rounded transition-all'>
           <img src='/icons/dots-grid.svg' title='Menu' />
         </button>
         <h1 className='text-xl leading-7 font-semibold'>Test Project</h1>
