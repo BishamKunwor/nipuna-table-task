99ea506febfbce52c7988a0cac0dab5313c9e985
Bisham Kunwor <novelian.nova@gmail.com>
2023-09-21 08:04:19 +0545

    Componentize Root Styles

---
+++ src/components/RootStyles/index.tsx
@@ -0,0 +1,46 @@
+import { StyleProvider } from '@ant-design/cssinjs'
+import { ConfigProvider } from 'antd'
+import { ReactNode } from 'react'
+
+export default function RootStyles ({ children }: { children: ReactNode }) {
+  return (
+    <StyleProvider hashPriority='high'>
+      <ConfigProvider
+        theme={{
+          token: {
+            borderRadius: 4,
+            colorPrimary: '#7474c9',
+            fontFamily:
+              "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','Noto Color Emoji'",
+            fontSize: 12,
+            colorTextPlaceholder: 'var(--h4)',
+            colorBorder: 'var(--border)'
+          },
+          components: {
+            Button: {},
+            Select: {
+              colorText: 'var(--h4)'
+            },
+            Table: {
+              borderColor: 'var(--divider)',
+              headerBg: 'transparent',
+              colorBgContainer: 'transparent',
+              filterDropdownBg: 'white',
+              headerColor: 'var(--h2-highlight)',
+              headerFilterHoverBg: 'var(--hover)',
+              headerSortActiveBg: 'var(--hover)',
+              headerSortHoverBg: 'var(--hover)',
+              rowHoverBg: 'var(--hover)',
+              colorFillAlter: '#ebebf8',
+              colorText: 'var(--h4)',
+              lineWidth: 0.5,
+              rowSelectedBg: 'transparent'
+            }
+          }
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </StyleProvider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,48 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { StyleProvider } from '@ant-design/cssinjs'
-import { ConfigProvider } from 'antd'
+import RootStyles from './components/RootStyles/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StyleProvider hashPriority='high'>
-      <ConfigProvider
-        theme={{
-          token: {
-            borderRadius: 4,
-            colorPrimary: '#7474c9',
-            fontFamily:
-              "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','Noto Color Emoji'",
-            fontSize: 12,
-            colorTextPlaceholder: 'var(--h4)',
-            colorBorder: 'var(--border)'
-          },
-          components: {
-            Button: {},
-            Select: {
-              colorText: 'var(--h4)'
-            },
-            Table: {
-              borderColor: 'var(--divider)',
-              headerBg: 'transparent',
-              colorBgContainer: 'transparent',
-              filterDropdownBg: 'white',
-              headerColor: 'var(--h2-highlight)',
-              headerFilterHoverBg: 'var(--hover)',
-              headerSortActiveBg: 'var(--hover)',
-              headerSortHoverBg: 'var(--hover)',
-              rowHoverBg: 'var(--hover)',
-              colorFillAlter: '#ebebf8',
-              colorText: 'var(--h4)',
-              lineWidth: 0.5,
-              rowSelectedBg: 'transparent'
-            }
-          }
-        }}
-      >
-        <App />
-      </ConfigProvider>
-    </StyleProvider>
+    <RootStyles>
+      <App />
+    </RootStyles>
   </React.StrictMode>
 )
