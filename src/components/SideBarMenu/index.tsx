892b437638aaa39ba7419e3776a902fa71d75da2
Bisham Kunwor <novelian.nova@gmail.com>
2023-09-21 05:51:02 +0545

    Completed Styling of SideNavBar

--- src/components/SideBarMenu/index.tsx
+++ src/components/SideBarMenu/index.tsx
@@ -1,4 +1,4 @@
-import { ConfigProvider, Input } from 'antd'
+import { Input } from 'antd'
 import { ReactNode } from 'react'
 
 const sideBarMenu = [
@@ -43,59 +43,51 @@ export default function SideBarMenu ({ children }: { children?: ReactNode }) {
     <section className='flex mt-2 h-[calc(100vh-80px)]'>
       <aside className='bg-white mr-2 relative'>
         <header className='pl-6 py-3 flex gap-2'>
-          <h2>CRM</h2>
-          <button>
+          <h3 className='text-[var(--h3)]'>CRM</h3>
+          <button className='hover:bg-[var(--hover)] transition-all'>
             <img src='/icons/menu/chevron-down.svg' title='Collapse Menu' />
           </button>
         </header>
-        <button className='bg-slate-300 absolute top-0 right-0'>
+        <button className='bg-[var(--toggle-icon-grey)] absolute top-0 right-0'>
           <img src='/icons/menu/chevron-left.svg' title='Collapse SideNav' />
         </button>
         <main className='p-6 pt-2'>
-          <ConfigProvider
-            theme={{
-              components: {
-                Input: {
-                  borderRadius: 4
-                }
-              }
-            }}
-          >
-            <Input
-              className='px-3 py-2'
-              placeholder='Search'
-              prefix={
-                <svg
-                  className='mr-2'
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    d='M21 21L17.5001 17.5M20 11.5C20 16.1944 16.1944 20 11.5 20C6.80558 20 3 16.1944 3 11.5C3 6.80558 6.80558 3 11.5 3C16.1944 3 20 6.80558 20 11.5Z'
-                    stroke='#667085'
-                    stroke-width='1.5'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                  />
-                </svg>
-              }
-              suffix={<img src='/icons/dots-horizontal.svg' title='Menu' />}
-            />
-          </ConfigProvider>
+          <Input
+            className='px-3 py-2'
+            placeholder='Search'
+            prefix={
+              <svg
+                className='mr-2'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M21 21L17.5001 17.5M20 11.5C20 16.1944 16.1944 20 11.5 20C6.80558 20 3 16.1944 3 11.5C3 6.80558 6.80558 3 11.5 3C16.1944 3 20 6.80558 20 11.5Z'
+                  stroke='#667085'
+                  stroke-width='1.5'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                />
+              </svg>
+            }
+            suffix={<img src='/icons/dots-horizontal.svg' title='Menu' />}
+          />
           <ul className='min-w-[192px] mt-6'>
             {sideBarMenu.map(menuItem => {
               return (
                 <li>
                   <a
                     href={menuItem.url || '#'}
-                    className='px-3 py-2 hover:bg-red-300 rounded-sm transition-all text-sm flex justify-between'
+                    className='px-3 py-2 hover:bg-[var(--hover)] rounded-sm transition-all text-sm flex justify-between'
                   >
                     <aside className='flex gap-3 items-center'>
                       <img src={menuItem.iconUrl} title={menuItem.title} />
-                      <p className='text-xs'>{menuItem.title}</p>
+                      <h3 className='text-xs text-[var(--h3)]'>
+                        {menuItem.title}
+                      </h3>
                     </aside>
                     <img src='/icons/dots-horizontal.svg' title='Menu' />
                   </a>
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { StyleProvider } from '@ant-design/cssinjs';
+import { StyleProvider } from '@ant-design/cssinjs'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StyleProvider hashPriority='high'>
-      <App />
+      <ConfigProvider
+        theme={{
+          token: {
+            borderRadius: 4,
+            colorPrimary: '#7474c9'
+          },
+          components: {
+            Button: {
+            },
+            Input: {}
+          }
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </StyleProvider>
   </React.StrictMode>
 )
